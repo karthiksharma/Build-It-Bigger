c386da4438f7e7a49ed32464d503b3ef5a9dfb35
Karthik Sharma <karthsharma@gmail.com>
2016-08-28 16:14:41 +0530

    Init project and Add joke rendering java library

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,9 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.karthik.udacity.JokeSmith;
 
 
 /**
@@ -31,6 +33,12 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+        root.findViewById(R.id.joke_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), new JokeSmith().getJoke(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return root;
     }
 }
---
+++ FinalProject/jokewizard/src/main/java/com/karthik/udacity/JokeSmith.java
@@ -0,0 +1,7 @@
+package com.karthik.udacity;
+
+public class JokeSmith {
+    public String getJoke(){
+        return " This is joke ";
+    }
+}
